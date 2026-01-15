bcf25eefafe75d10d0b3f9f591e8f87805bdc931
Rupak Parui <catch2rupak@gmail.com>
2026-01-15 15:36:49 +0530

    add data validation component and schema definition for network security module

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from networksecurity.components.data_ingestion import DataIngestion
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig
 from networksecurity.logging.logger import logging
 from networksecurity.exception.exception import NetworkSecurityException
 import sys
@@ -8,5 +9,13 @@ if __name__ == '__main__':
     trainingpipelineconfig = TrainingPipelineConfig()
     dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
     dataingestion = DataIngestion(dataingestionconfig)
+    logging.info("Stating data ingestion")
     artifact = dataingestion.initiate_data_ingestion()
     print(artifact)
+    logging.info("Data Ingestion completed")
+    datavalidationconfig = DataValidationConfig(trainingpipelineconfig)
+    logging.info("Starting data validation")
+    datavalidation = DataValidation(artifact, datavalidationconfig)
+    data_validation_artifact = datavalidation.initiate_data_validation()
+    logging.info("Data Validation completed")
+    print(data_validation_artifact)
